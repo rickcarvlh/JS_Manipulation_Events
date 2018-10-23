a7ed76dba576822b7170dceb9d0964f9bee8e95d
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-10-23 14:57:34 +0100

    Game init function

--- starter/app.js
+++ starter/app.js
@@ -16,29 +16,11 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-//	scores for both players 
-scores = [0, 0];
-
-roundScore = 0;
-// the default player is 0
-activePlayer = 0;
-
-
-/**hiding the dice trought the js no css -> display:none */
-// in js all styles are inline
-document.querySelector('.dice').style.display = 'none';
-
-// same as querySelector but faster
-//* setting everything to 0
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
+init();
 
 
 // there are a lot of event check MDN for more
-document.querySelector('.btn-roll').addEventListener('click', function() {
+document.querySelector('.btn-roll').addEventListener('click', function () {
 	// Do something here Anomymous function - can't be called anywhere else
 
 	//*1. Random Number
@@ -52,23 +34,23 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
 	//*3. Update the round score IF the rolled number was NOT a 1
-	if(dice !== 1){
+	if (dice !== 1) {
 		//Add Score
-		roundScore += dice;	//same has roundScore = roundScore + dice;
+		roundScore += dice; //same has roundScore = roundScore + dice;
 		// update the roundScore and display the roundScore
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
-	}else {
+	} else {
 		// Next player
 		// if it's 0 then activePlayer should be 1 else activePlayer should 0
-		nextPlayer();	
+		nextPlayer();
 
 	}
-	
+
 });
 
 // Event Listener
 
-document.querySelector('.btn-hold').addEventListener('click',	function(){
+document.querySelector('.btn-hold').addEventListener('click', function () {
 	//	Add CURRENT score to GLOBAL score
 	//* 	scores[activePlayer] = scores[activePlayer] + roundeScore; -> same thing
 	scores[activePlayer] += roundScore;
@@ -77,43 +59,80 @@ document.querySelector('.btn-hold').addEventListener('click',	function(){
 	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
 	//	Check if player won the game
-	if(scores[activePlayer] >= 20){
+	if (scores[activePlayer] >= 20) {
 		// replace player name with winner
-		document.querySelector('#name-' + activePlayer).textContent	=	'Winner!';
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
 		// remove active class from winner
 		document.querySelector('.dice').style.display = 'none';
 		// classList gets acess to the classes of the element
 		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
 
-	}else{
+	} else {
 		// if the game continues we want the next player to play
 		//	Next 	Player 
-				nextPlayer();	
+		nextPlayer();
 	}
 
-	
+
 });
 
-// Don't repeat yourself
-function	nextPlayer(){
-		// Next player
-		// if it's 0 then activePlayer should be 1 else activePlayer should 0
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+//! Don't repeat yourself
+function nextPlayer() {
+	// Next player
+	// if it's 0 then activePlayer should be 1 else activePlayer should 0
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 
-		//so the score doesn't add up when switching players 
-		roundScore = 0;
+	//so the score doesn't add up when switching players 
+	roundScore = 0;
 
-		document.getElementById('current-0').textContent = '0';
-		document.getElementById('current-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
 
-		// this way it just toggles the classes . Changes between them
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
+	// this way it just toggles the classes . Changes between them
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
 
-		// when a player rolls a one hide the dice again
-		document.querySelector('.dice').style.display = 'none';
+	// when a player rolls a one hide the dice again
+	document.querySelector('.dice').style.display = 'none';
 }
+
+// listen to an event->EventListener
+//	New Game -> Reset player Scores(everything)
+//	Call the function 
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+
+// function to initialize the game
+function init() {
+
+	//	scores for both players 
+	scores = [0, 0];
+	roundScore = 0;
+	// the default player is 0
+	activePlayer = 0;
+
+	/**hiding the dice trought the js no css -> display:none */
+	// in js all styles are inline
+	document.querySelector('.dice').style.display = 'none';
+
+	// same as querySelector but faster
+	//* setting everything to 0
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+
+}
+
 
 
 // dice = Math.floor(Math.random() * 6)	+	1;
